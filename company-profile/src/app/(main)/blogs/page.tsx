55c92278c6b37eece0d8123a626ca029a03d4bd2
Kimgeovedii <mohamadmustofahakim@gmail.com>
2025-11-22 16:31:36 +0700

    `Added initial implementation of BlogPages component`

---
+++ company-profile/src/app/(main)/blogs/page.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+interface IBlogPagesProps {}
+
+const BlogPages: React.FunctionComponent<IBlogPagesProps> = (props) => {
+  return (
+    <div>
+      <h1>Blog Pages</h1>
+    </div>
+  );
+};
+
+export default BlogPages;
