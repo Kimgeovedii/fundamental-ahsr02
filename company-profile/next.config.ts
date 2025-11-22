0a5e41d11ea327c4102ff41b51b064f7aff00a73
Kimgeovedii <mohamadmustofahakim@gmail.com>
2025-11-22 16:01:01 +0700

    `Updated next.config.ts to include additional domain in images configuration and removed unused page component`

--- company-profile/next.config.ts
+++ company-profile/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["fiwqmjpomadripodbyyq.supabase.co"],
+    domains: ["fiwqmjpomadripodbyyq.supabase.co", "placeholder.com"],
   },
 };
 
